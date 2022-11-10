81d3cf05ba338239ad1feb7bfc816255567c7b9f
Chris Hyde <c_hyde333@hotmail.com>
2022-11-10 12:11:33 +0000

    Adding problems to 1. & 2.

--- practice.js
+++ practice.js
@@ -1,4 +1,26 @@
+// Codewars & Leetcode problems
+
 // 1. Two Sum
+
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1] */
 
 // Leet Code
 
@@ -15,6 +37,27 @@ const twoSum = function (numbers, target) {
 };
 
 // 2. Valid Parentheses
+
+/* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false */
 
 const isValid = function (s) {
   let bracket = [];
