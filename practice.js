19b460a80a60d47c7149ffc9ada6c4d9dc4b9b04
Chris Hyde <c_hyde333@hotmail.com>
2022-11-12 13:58:17 +0000

    Question 6

--- practice.js
+++ practice.js
@@ -220,3 +220,35 @@ const isPalindrome = function (s) {
   }
   return true;
 };
+
+// .6
+
+/* Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+
+Input: root = []
+Output: [] */
+
+function invertTree(root) {
+  if (!root) return root;
+
+  const right = root.right;
+  root.right = root.left;
+  root.left = right;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
