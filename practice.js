59abf9a018373c5bdc7745ad9f76a7705e1781a6
Chris Hyde <c_hyde333@hotmail.com>
2022-11-13 15:15:34 +0000

    7. Leet

--- practice.js
+++ practice.js
@@ -252,3 +252,32 @@ function invertTree(root) {
 
   return root;
 }
+
+// 7. Valid Anagram
+
+/* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+*/
+
+const isAnagram = function (s, t) {
+  s = s.split("").sort();
+  t = t.split("").sort();
+
+  if (s.length !== t.length) return false;
+
+  for (var i = 0; i < s.length; i++) if (s[i] !== t[i]) return false;
+
+  return true;
+};
