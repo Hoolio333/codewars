59294dd29e5970a03a9da9a47e8956459461a139
Chris Hyde <c_hyde333@hotmail.com>
2022-11-14 12:29:51 +0000

    8. Leet

--- practice.js
+++ practice.js
@@ -281,3 +281,36 @@ const isAnagram = function (s, t) {
 
   return true;
 };
+
+// 8. Binary Search
+/* Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1 */
+
+const search = function (arr, target) {
+  let n = arr.length;
+  let i = 0;
+  let j = n - 1;
+
+  while (i <= j) {
+    let mid = parseInt((i + j) / 2);
+    if (arr[mid] == target) return mid;
+    if (arr[mid] < target) i = mid + 1;
+    else j = mid - 1;
+  }
+
+  return -1;
+};
