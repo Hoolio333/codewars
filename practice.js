829588cbfa6b88eef21cedaacc782e836510ef9e
Chris Hyde <c_hyde333@hotmail.com>
2022-11-15 15:16:55 +0000

    No. 9 Leet

--- practice.js
+++ practice.js
@@ -287,8 +287,6 @@ const isAnagram = function (s, t) {
 
 You must write an algorithm with O(log n) runtime complexity.
 
- 
-
 Example 1:
 
 Input: nums = [-1,0,3,5,9,12], target = 9
@@ -313,4 +311,46 @@ const search = function (arr, target) {
   }
 
   return -1;
+};
+
+/* 9. Flood Fill
+
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+
+Example 1:
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Output: [[0,0,0],[0,0,0]]
+Explanation: The starting pixel is already colored 0, so no changes are made to the image. */
+
+const floodFill = function (image, sr, sc, newColor) {
+  let value = image[sr][sc];
+  if (value === newColor) {
+    return image;
+  }
+  let row = image.length;
+  let colm = image[0].length;
+  fill(sr, sc);
+  return image;
+  function fill(i, j) {
+    if (i < 0 || j < 0 || i === row || j === colm || image[i][j] !== value) {
+      return;
+    }
+    image[i][j] = newColor;
+    fill(i + 1, j);
+    fill(i - 1, j);
+    fill(i, j + 1);
+    fill(i, j - 1);
+  }
 };
