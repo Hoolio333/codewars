b8083e66e383df2259e0204317a225a7d6dcfb47
Chris Hyde <c_hyde333@hotmail.com>
2022-11-21 15:56:09 +0000

    15. Leet

--- practice.js
+++ practice.js
@@ -587,3 +587,40 @@ const solution = function (isBadVersion) {
     return mid;
   };
 };
+
+/* 15. Ransom Note
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+*/
+
+const canConstruct = (ransomNote, magazine) => {
+  const vocab = {};
+
+  for (let letter of magazine) {
+    vocab[letter] = ++vocab[letter] || 1;
+  }
+
+  for (let letter of ransomNote) {
+    if (vocab[letter] === 0 || !vocab[letter]) {
+      return false;
+    }
+    vocab[letter]--;
+  }
+
+  return true;
+};
