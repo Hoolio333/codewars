c5a804a1ef9644941d8d7b52ea6bfc34ff8030bd
Chris Hyde <c_hyde333@hotmail.com>
2022-11-23 17:22:41 +0000

    17. Leet

--- practice.js
+++ practice.js
@@ -659,3 +659,43 @@ const climbStairs = function (n) {
   }
   return ans[n - 1];
 };
+
+/* 17. Longest Palindrome
+
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+ 
+
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+Example 2:
+
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1. */
+
+const longestPalindrome = function (s) {
+  const set = new Set();
+
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (set.has(s[i])) {
+      count += 2;
+      set.delete(s[i]);
+    } else {
+      set.add(s[i]);
+    }
+  }
+
+  if (set.size > 0) {
+    count++;
+  }
+
+  return count;
+};
