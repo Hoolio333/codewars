8a81b0d4c35cafe254bf48978e20ec7d236b880c
Chris Hyde <c_hyde333@hotmail.com>
2022-11-29 17:15:39 +0000

    23. Leet

--- practice.js
+++ practice.js
@@ -854,3 +854,38 @@ const middleNode = function (head) {
   }
   return slow;
 };
+
+/* 23. Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+*/
+
+const maxDepth = function (root) {
+  if (!root?.val && root?.val !== 0) return 0;
+  let output = 0;
+
+  const loop = (tree, level) => {
+    output = level > output ? level : output;
+
+    if (tree?.left) {
+      loop(tree.left, level + 1);
+    }
+    if (tree?.right) {
+      loop(tree.right, level + 1);
+    }
+  };
+  loop(root, 1);
+
+  return output;
+};
