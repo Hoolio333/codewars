db2ce32fafa0c4bd28ac2143ca0c87a99b6cdcde
Chris Hyde <c_hyde333@hotmail.com>
2022-12-02 13:56:24 +0000

    Code Wars isPalindrome

--- practice.js
+++ practice.js
@@ -952,3 +952,33 @@ const noBoringZeros = (number) => {
   if (number % 10 === 0) return noBoringZeros(number / 10);
   else return number;
 };
+
+/* Code Wars
+
+Level 8 - Is it a palindrome?
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+describe("Fixed tests", function() {
+  it("Testing for 'a'", () => assert.strictEqual(isPalindrome("a"), true));
+  it("Testing for 'aba'", () => assert.strictEqual(isPalindrome("aba"), true));
+  it("Testing for 'Abba'", () => assert.strictEqual(isPalindrome("Abba"), true));
+  it("Testing for 'hello'", () => assert.strictEqual(isPalindrome("hello"), false));
+  it("Testing for 'Bob'", () => assert.strictEqual(isPalindrome("Bob"), true));
+  it("Testing for 'Madam'", () => assert.strictEqual(isPalindrome("Madam"), true));
+  it("Testing for 'AbBa'", () => assert.strictEqual(isPalindrome("AbBa"), true));
+  it("Testing for ''", () => assert.strictEqual(isPalindrome(""), true));
+});
+*/
+
+function isPalindrome(str) {
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, "");
+  var len = str.length;
+  for (var i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
