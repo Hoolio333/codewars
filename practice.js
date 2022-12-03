d95cb613e7a2bd5690e8db2598a0be839b813be4
Chris Hyde <c_hyde333@hotmail.com>
2022-12-03 23:23:53 +0000

    Code Wars - Count the divisors of a number

--- practice.js
+++ practice.js
@@ -1015,3 +1015,37 @@ function dutyFree(normPrice, discount, hol) {
 function dutyFree(normPrice, discount, hol) {
   return Math.floor((hol / normPrice / discount) * 100);
 }
+
+/* Code Wars
+
+Level 7 - Count the divisors of a number
+
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(getDivisorsCnt(1),  1);
+    assert.strictEqual(getDivisorsCnt(10), 4);
+    assert.strictEqual(getDivisorsCnt(11), 2);
+    assert.strictEqual(getDivisorsCnt(54), 8);
+  });
+}); */
+
+function getDivisorsCnt(n) {
+  let output = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      output += 1;
+    }
+  }
+  return output;
+}
