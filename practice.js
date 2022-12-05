fd3b16afd83b54aaed7ae9a7e8c8caa217fd6b09
Chris Hyde <c_hyde333@hotmail.com>
2022-12-05 16:42:30 +0000

    Code Wars Binary Addition

--- practice.js
+++ practice.js
@@ -1090,3 +1090,42 @@ function enough(cap, on, wait) {
 function enough(cap, on, wait) {
   return cap - wait - on < 0 ? on + wait - cap : 0;
 }
+
+/* Code Wars
+
+Level 7 - Binary Addition
+
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+describe("addBinary(1,2)", function() {
+  var results1 = addBinary(1,2);
+  it("Should return something that isn't falsy", function() {
+    Test.expect(results1, "Something is wrong, no results!");
+  });
+  it("Should return \"11\"", function() {
+    Test.assertEquals(results1, "11");
+  });
+}); */
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
+
+// Alternative Solution:
+
+function addBinary(a, b) {
+  var c = a + b;
+  var res = "";
+  while (c >= 1) {
+    var res = (c % 2) + res;
+    c = Math.floor(c / 2);
+  }
+  return res;
+}
