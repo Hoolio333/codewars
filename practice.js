7865cba0a0fcfcfa514e62e9fe21cef03e7c25df
Chris Hyde <c_hyde333@hotmail.com>
2022-12-06 14:16:10 +0000

    Codewars - Exes and Ohs

--- practice.js
+++ practice.js
@@ -1129,3 +1129,48 @@ function addBinary(a, b) {
   }
   return res;
 }
+
+/* Code Wars
+
+Level 7 - Exes and Ohs
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(XO('xo'),true);
+Test.assertEquals(XO("xxOo"),true);
+Test.assertEquals(XO("xxxm"),false);
+Test.assertEquals(XO("Oo"),false);
+Test.assertEquals(XO("ooom"),false);
+  });
+}); */
+
+function XO(str) {
+  var x = str.match(/x/gi);
+  var o = str.match(/o/gi);
+  return (x && x.length) == (o && o.length);
+}
+
+// Alternative Solutions:
+
+function XO(str) {
+  return (
+    str.toLowerCase().split("x").length === str.toLowerCase().split("o").length
+  );
+}
+
+const XO = (str) => {
+  str = str.toLowerCase().split("");
+  return (
+    str.filter((x) => x === "x").length === str.filter((x) => x === "o").length
+  );
+};
