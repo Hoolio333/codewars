e5cda064c7f223fdc18acd36ffb47a5b77530df5
Chris Hyde <c_hyde333@hotmail.com>
2022-12-07 14:08:48 +0000

    Code Wars Ones and Zeros

--- practice.js
+++ practice.js
@@ -1174,3 +1174,75 @@ const XO = (str) => {
     str.filter((x) => x === "x").length === str.filter((x) => x === "o").length
   );
 };
+
+/* Code Wars
+
+Level 7 - Ones and Zeros
+
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+describe("One's and Zero's", () => {
+
+  it("Example tests", () => {
+      Test.assertEquals(binaryArrayToNumber([0,0,0,1]), 1);
+      Test.assertEquals(binaryArrayToNumber([0,0,1,0]), 2);
+      Test.assertEquals(binaryArrayToNumber([1,1,1,1]), 15);
+      Test.assertEquals(binaryArrayToNumber([0,1,1,0]), 6);
+  });
+
+}); */
+
+function binaryArrayToNumber(arr) {
+  return Number.parseInt(arr.join(""), 2);
+}
+
+// Alternative Solutions
+
+function binaryArrayToNumber(s) {
+  if (s[0] === 0 && s[1] === 0 && s[2] === 0 && s[3] === 1) {
+    return 1;
+  } else if (s[0] === 0 && s[1] === 0 && s[2] === 1 && s[3] === 0) {
+    return 2;
+  } else if (s[0] === 0 && s[1] === 0 && s[2] === 1 && s[3] === 1) {
+    return 3;
+  } else if (s[0] === 0 && s[1] === 1 && s[2] === 0 && s[3] === 0) {
+    return 4;
+  } else if (s[0] === 0 && s[1] === 1 && s[2] === 0 && s[3] === 1) {
+    return 5;
+  } else if (s[0] === 0 && s[1] === 1 && s[2] === 1 && s[3] === 0) {
+    return 6;
+  } else if (s[0] === 0 && s[1] === 1 && s[2] === 1 && s[3] === 1) {
+    return 7;
+  } else if (s[0] === 1 && s[1] === 0 && s[2] === 0 && s[3] === 0) {
+    return 8;
+  } else if (s[0] === 1 && s[1] === 0 && s[2] === 0 && s[3] === 1) {
+    return 9;
+  } else if (s[0] === 1 && s[1] === 0 && s[2] === 1 && s[3] === 0) {
+    return 10;
+  } else if (s[0] === 1 && s[1] === 0 && s[2] === 1 && s[3] === 1) {
+    return 11;
+  } else if (s[0] === 1 && s[1] === 1 && s[2] === 0 && s[3] === 0) {
+    return 12;
+  } else if (s[0] === 1 && s[1] === 1 && s[2] === 0 && s[3] === 1) {
+    return 13;
+  } else if (s[0] === 1 && s[1] === 1 && s[2] === 1 && s[3] === 0) {
+    return 14;
+  } else if (s[0] === 1 && s[1] === 1 && s[2] === 1 && s[3] === 1) {
+    return 15;
+  } else if (s[0] === 0 && s[1] === 0 && s[2] === 0 && s[3] === 0) {
+    return 0;
+  }
+}
