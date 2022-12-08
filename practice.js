851acaf98f1ee26b2ee24436c43938b08a8a57f9
Chris Hyde <c_hyde333@hotmail.com>
2022-12-08 17:10:37 +0000

    Code Wars - DNA

--- practice.js
+++ practice.js
@@ -1270,3 +1270,72 @@ describe("Tests", () => {
 function numberToString(num) {
   return num.toString();
 }
+
+/* Code Wars
+
+Level 7 - Complementary DNA
+
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(DNAStrand("AAAA"),"TTTT","String AAAA is")
+    assert.strictEqual(DNAStrand("ATTGC"),"TAACG","String ATTGC is")
+    assert.strictEqual(DNAStrand("GTAT"),"CATA","String GTAT is")   
+  })
+}) */
+
+function DNAStrand(dna) {
+  let sequence = {
+    A: "T",
+    T: "A",
+    G: "C",
+    C: "G",
+  };
+  return dna.replace(/A|T|G|C/g, function (matched) {
+    return sequence[matched];
+  });
+}
+
+// Alternative Solutions:
+
+var pairs = { A: "T", T: "A", C: "G", G: "C" };
+
+function DNAStrand(dna) {
+  return dna
+    .split("")
+    .map(function (v) {
+      return pairs[v];
+    })
+    .join("");
+}
+
+function DNAStrand(dna) {
+  //your code here
+  var result = "";
+  for (var i = 0; i < dna.length; i++) {
+    if (dna[i] === "A") {
+      result += "T";
+    } else if (dna[i] === "T") {
+      result += "A";
+    } else if (dna[i] === "C") {
+      result += "G";
+    } else if (dna[i] === "G") {
+      result += "C";
+    } else {
+      result += dna[i];
+    }
+  }
+  return result;
+}
