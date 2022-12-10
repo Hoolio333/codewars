c3925058acc5e7223c40ce3368c621c41638df6f
Chris Hyde <c_hyde333@hotmail.com>
2022-12-10 14:08:22 +0000

    Code Wars - Odd or Evan

--- practice.js
+++ practice.js
@@ -1377,3 +1377,99 @@ function sortByLength(array) {
 // Alternative Solutions:
 
 let sortByLength = (arr) => arr.sort((a, b) => a.length - b.length);
+
+/* Code Wars
+
+Level 7 - Odd or Even?
+
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+
+describe('Fixed tests', () => {
+  it('Edge tests', () => {
+    assert.strictEqual(oddOrEven([0]), 'even')
+    assert.strictEqual(oddOrEven([1]), 'odd')
+    assert.strictEqual(oddOrEven([]), 'even')
+  });
+  
+  it('Even tests', () => {
+    assert.strictEqual(oddOrEven([0, 1, 5]), 'even')
+    assert.strictEqual(oddOrEven([0, 1, 3]), 'even')
+    assert.strictEqual(oddOrEven([1023, 1, 2]), 'even')
+  });
+  
+  it('Negative Even tests', () => {
+    assert.strictEqual(oddOrEven([0, -1, -5]), 'even')
+    assert.strictEqual(oddOrEven([0, -1, -3]), 'even')
+    assert.strictEqual(oddOrEven([-1023, 1, -2]), 'even')
+  });
+  
+  it('Odd tests', () => {
+    assert.strictEqual(oddOrEven([0, 1, 2]), 'odd')
+    assert.strictEqual(oddOrEven([0, 1, 4]), 'odd')
+    assert.strictEqual(oddOrEven([1023, 1, 3]), 'odd')
+  });
+  
+  it('Negative Odd tests', () => {
+    assert.strictEqual(oddOrEven([0, -1, 2]), 'odd')
+    assert.strictEqual(oddOrEven([0, 1, -4]), 'odd')
+    assert.strictEqual(oddOrEven([-1023, -1, 3]), 'odd')
+  });
+}); */
+
+function oddOrEven(array) {
+  return array.reduce(function (sum, item) {
+    return sum + item;
+  }, 0) %
+    2 ==
+    0
+    ? "even"
+    : "odd";
+}
+
+// Alternative Solutions:
+
+function oddOrEven(arr) {
+  return arr.reduce((a, b) => a + b, 0) % 2 ? "odd" : "even";
+}
+
+function oddOrEven(array) {
+  var result = 0;
+  for (var i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  if (result % 2 == 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+
+function oddOrEven(array) {
+  if (array.length == 0) {
+    return "even";
+  }
+  var sum = array.reduce(add, 0);
+  function add(x, y) {
+    return x + y;
+  }
+
+  if (sum % 2 == 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
