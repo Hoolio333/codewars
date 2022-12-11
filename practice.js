89252f0acd4850d992702958a8c248534f1710f0
Chris Hyde <c_hyde333@hotmail.com>
2022-12-11 14:53:04 +0000

    Code Wars Given the triangle of consecutive odd numbers

--- practice.js
+++ practice.js
@@ -1504,3 +1504,53 @@ function otherAngle(a, b) {
 function otherAngle(a, b) {
   return 180 - (a + b);
 }
+
+/* Code Wars
+
+Level 7 - Given the triangle of consecutive odd numbers
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(rowSumOddNumbers(1), 1);
+    assert.strictEqual(rowSumOddNumbers(42), 74088);
+  });
+}); 
+*/
+
+function rowSumOddNumbers(n) {
+  return n * n * n;
+}
+
+// Alternative Solutions:
+
+function rowSumOddNumbers(n) {
+  var start = n * n - n + 1;
+  var result = 0;
+
+  for (i = 0; i < n; i++) {
+    result = result + (start + i * 2);
+  }
+
+  return result;
+}
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+function rowSumOddNumbers(n) {
+  // TODO
+  return n > 0 ? n * n * n : "Wrong Input";
+}
