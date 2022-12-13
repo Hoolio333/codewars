e12bbcc7bf5d8b8e3d9fbd6b7c07d1c665d64cda
Chris Hyde <c_hyde333@hotmail.com>
2022-12-13 12:15:03 +0000

    if else and ternary operator

--- practice.js
+++ practice.js
@@ -1838,3 +1838,84 @@ function friend(friends) {
   }
   return goodFriends;
 }
+
+/* Code Wars
+
+Level 7 - Training JS #7: if..else and ternary operator
+
+This function returns a different value depending on the parameter age.
+
+Looks very complicated? Well, JS and Ruby also support the ternary operator and Python has something similar too:
+
+condition ? statementa : statementb
+Condition and statement separated by "?", different statement separated by ":" in both Ruby and JS; in Python you put the condition in the middle of two alternatives. The two examples above can be simplified with ternary operator:
+
+function oddEven(n){
+  return n%2 == 1 ? "odd number" : "even number";
+}
+function oldYoung(age){
+  return age < 16 ? "children" : age < 50 ? "young man" : "old man";
+}
+Task:
+Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+
+number of hotdogs	price per unit (cents)
+n < 5	100
+n >= 5 and n < 10	95
+n >= 10	90
+
+You can use if..else or ternary operator to complete it.
+
+describe("Tests", () => {
+  it("Sample tests", () => {
+    assert.strictEqual(saleHotdogs(  1),  100);
+    assert.strictEqual(saleHotdogs(  4),  400);
+    assert.strictEqual(saleHotdogs(  5),  475);
+    assert.strictEqual(saleHotdogs(  9),  855);
+    assert.strictEqual(saleHotdogs( 10),  900);
+    assert.strictEqual(saleHotdogs(100), 9000);
+  });
+});
+*/
+
+function saleHotdogs(n) {
+  return n * (n < 5 ? 100 : n >= 5 && n < 10 ? 95 : 90);
+}
+
+// Alternative Solutions:
+
+function saleHotdogs(n) {
+  return n * (n < 5 ? 100 : n < 10 ? 95 : 90);
+}
+
+function saleHotdogs(n) {
+  var money = 0;
+  if (n < 5) {
+    money = n * 100;
+  } else if (n >= 5 && n < 10) {
+    money = n * 95;
+  } else if (n >= 10) {
+    money = n * 90;
+  }
+  return money;
+}
+
+const saleHotdogs = (n) => {
+  switch (true) {
+    case n < 5:
+      return n * 100;
+    case n < 10:
+      return n * 95;
+    default:
+      return n * 90;
+  }
+};
+
+function saleHotdogs(n) {
+  if (n < 5) return n * 100;
+  if (n >= 5 && n < 10) return n * 95;
+  else return n * 90;
+}
+
+const saleHotdogs = (customersNumber) =>
+  customersNumber * (customersNumber < 5 ? 100 : customersNumber > 9 ? 90 : 95);
