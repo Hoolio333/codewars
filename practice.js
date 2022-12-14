f36155fba4a566e058ed08be26c5ac5b0a6881ee
Chris Hyde <c_hyde333@hotmail.com>
2022-12-14 11:51:07 +0000

    Code Wars Round to nearest 5

--- practice.js
+++ practice.js
@@ -1919,3 +1919,53 @@ function saleHotdogs(n) {
 
 const saleHotdogs = (customersNumber) =>
   customersNumber * (customersNumber < 5 ? 100 : customersNumber > 9 ? 90 : 95);
+
+/* Code Wars
+
+Level 7 - Round up to the next multiple of 5
+
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    [[0,0],[1,5],[-1,0],[-5,-5],[3,5],[5,5],[7,10],[20,20],[39,40],[990,990],[121,125],[555,555]].forEach(
+      ([x,exp])=> assert.strictEqual(roundToNext5(x), exp, `Input: ${x}`)
+    );
+  })
+});
+*/
+
+function roundToNext5(n) {
+  return Math.ceil(n / 5) * 5;
+}
+
+// Alternative Solutions:
+
+function roundToNext5(n) {
+  while (n % 5 !== 0) n++;
+  return n;
+}
+
+function roundToNext5(n) {
+  if (n % 5 === 0) {
+    return n;
+  }
+  return roundToNext5(n + 1);
+}
+
+function roundToNext5(n) {
+  return n % 5 === 0 ? n : n < 0 ? n - (n % 5) : n - (n % 5) + 5;
+}
