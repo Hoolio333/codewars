fe69301c2e58fb1b90b04fe70c871e4c648c07d7
Chris Hyde <c_hyde333@hotmail.com>
2022-12-15 12:49:31 +0000

    Code Wars - Two to one

--- practice.js
+++ practice.js
@@ -1969,3 +1969,62 @@ function roundToNext5(n) {
 function roundToNext5(n) {
   return n % 5 === 0 ? n : n < 0 ? n - (n % 5) : n - (n % 5) + 5;
 }
+
+/* Code Wars
+
+Level 7 - Two to One
+
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+describe("longest",function() {
+it("Basic tests",function() {
+    Test.assertEquals(longest("aretheyhere", "yestheyarehere"), "aehrsty")
+    Test.assertEquals(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
+    Test.assertEquals(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
+})})
+*/
+
+function longest(s1, s2) {
+  return [...new Set(s1 + s2)].sort().join``;
+}
+
+// Alternative Solutions:
+
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2))
+    .sort()
+    .join("");
+}
+
+function longest(s1, s2) {
+  // your code
+  s3 = s1 + s2;
+  s4 = s3.split("");
+  s4 = s4.sort().filter(function (element, index, array) {
+    return element !== array[index - 1];
+  });
+  return s4.join("");
+}
+
+function longest(s1, s2) {
+  let output = [];
+  let combi = s1.concat(s2);
+  let array = combi.split("").sort();
+
+  for (let i = 0; i <= array.length; i++) {
+    console.log(array[i]); //test
+    if (!output.includes(array[i])) {
+      output.push(array[i]);
+    }
+  }
+  return output.join("");
+}
+console.log(longest("xyaaAbbbccccdefww", "xxxxyyyyabklmopq"));
