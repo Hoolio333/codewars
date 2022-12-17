1f0de8947ff9eb82f48ccb52e9880d7ef7a7e064
Chris Hyde <c_hyde333@hotmail.com>
2022-12-17 17:00:24 +0000

    Remove String Spaces

--- practice.js
+++ practice.js
@@ -2289,3 +2289,38 @@ function abbrevName(name) {
     .map((el) => el[0].toUpperCase())
     .join(".");
 }
+
+/* Code Wars
+
+Level 8 - Remove String Spaces
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+describe("Basic tests",() =>{
+  it("Fixed Tests", () => {
+    assert.strictEqual(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB');
+    assert.strictEqual(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd'); 
+    assert.strictEqual(noSpace('8aaaaa dddd r     '), '8aaaaaddddr'); 
+  });
+});
+*/
+
+function noSpace(x) {
+  let newStr = "";
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== " ") {
+      newStr += x[i];
+    }
+  }
+  return newStr;
+}
+
+// Alternative Solutions
+
+function noSpace(x) {
+  return x.replace(/\s/g, "");
+}
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
