5650cd1c51e42951ac5950382da4c9d47d56dffe
Chris Hyde <c_hyde333@hotmail.com>
2022-12-17 14:28:21 +0000

    Code wars List Filtering

--- practice.js
+++ practice.js
@@ -2076,3 +2076,41 @@ function removeEveryOther(arr) {
 }
 
 const removeEveryOther = (arr) => arr.filter((item, i) => i % 2 == 0);
+
+/* Code Wars
+
+Level 7 - List Filtering
+
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.deepEqual(filter_list([1,2,'a','b']),[1,2], 'For input [1,2,"a","b"]');
+    assert.deepEqual(filter_list([1,'a','b',0,15]),[1,0,15], 'For input [1,"a","b",0,15]');
+    assert.deepEqual(filter_list([1,2,'aasf','1','123',123]),[1,2,123], 'For input [1,2,"aasf","1","123",123]');
+  });
+});
+*/
+
+function filter_list(l) {
+  let integers = [];
+  for (var i = 0; i < l.length; i++) {
+    if (Number.isInteger(l[i])) {
+      integers.push(l[i]);
+    }
+  }
+  return integers;
+}
+
+// Alternative Solutions:
+
+function filter_list(l) {
+  return l.filter(function (v) {
+    return typeof v == "number";
+  });
+}
