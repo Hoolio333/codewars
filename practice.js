6327f89f0d9d9350067fc3e173165601f90ccbe0
Chris Hyde <c_hyde333@hotmail.com>
2022-12-17 17:03:53 +0000

    Code Wars - Disemvowel Trolls

--- practice.js
+++ practice.js
@@ -2324,3 +2324,70 @@ function noSpace(x) {
 function noSpace(x) {
   return x.split(" ").join("");
 }
+
+/* Code Wars
+
+Level 7 - Disemvowel Trolls
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!")
+    assert.strictEqual(disemvowel("No offense but,\nYour writing is among the worst I've ever read"), "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")
+    assert.strictEqual(disemvowel("What are you, a communist?"), "Wht r y,  cmmnst?")
+    })
+  })
+  */
+
+function disemvowel(str) {
+  return (str || "").replace(/[aeiou]/gi, "");
+}
+
+// Alternative Solutions
+
+disemvowel = (str) => str.replace(/[aeiou]/gi, "");
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, "");
+}
+
+const vowels = "aeiou";
+
+function disemvowel(str) {
+  return str
+    .split("")
+    .filter((letter) => !vowels.includes(letter.toLowerCase()))
+    .join("");
+}
+
+function disemvowel(str) {
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  return str
+    .split("")
+    .filter(function (el) {
+      return vowels.indexOf(el.toLowerCase()) == -1;
+    })
+    .join("");
+}
+
+const disemvowel = (str) => {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let newStr = "";
+  for (let i = 0; i <= str.length; i++) {
+    let char = str.charAt(i);
+    if (vowels.indexOf(char) == -1) {
+      newStr += char;
+    }
+  }
+  return newStr;
+};
