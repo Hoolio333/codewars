283a17e6249d48ace98c3e24e1ba2d913efb23e3
Chris Hyde <c_hyde333@hotmail.com>
2022-12-17 16:53:55 +0000

    Printer Errors

--- practice.js
+++ practice.js
@@ -2114,3 +2114,129 @@ function filter_list(l) {
     return typeof v == "number";
   });
 }
+
+/* Code Wars
+
+Level 6 - Unique In Order
+
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.deepEqual(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+  });
+});
+*/
+
+let uniqueInOrder = function (iterable) {
+  let arr = [];
+  for (let i = 0; i < iterable.length; i++) {
+    // check if there is back to back letter
+    if (iterable[i] !== iterable[i + 1]) {
+      // if not, push the current value to arr
+      arr.push(iterable[i]);
+    }
+  }
+  return arr;
+};
+
+/* Code Wars
+
+Level 7 - Printer Errors
+
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.
+
+Examples:
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
+
+describe("printerError",function() {
+it("Basic tests",function() {   
+    var s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+    Test.assertEquals(printerError(s), "3/56")
+})})
+*/
+
+function printerError(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] > "m") {
+      count++;
+    }
+  }
+  return count + "/" + str.length;
+}
+
+// Alternative Solutions:
+
+const printerError = (s) => `${s.replace(/[a-m]/gi, "").length}/${s.length}`;
+
+function printerError(s) {
+  var x = 0;
+  var y = 0;
+  for (var i = 0; i < s.length; i++) {
+    if (s.charAt(i).includes("n")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("o")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("p")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("q")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("r")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("s")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("t")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("u")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("v")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("w")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("x")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("y")) {
+      x++;
+      y++;
+    } else if (s.charAt(i).includes("z")) {
+      x++;
+      y++;
+    } else {
+      y++;
+    }
+  }
+  var xString = x.toString();
+  var yString = y.toString();
+  var fraction = "/";
+  return xString + fraction + yString;
+}
