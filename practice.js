72fcfd6f0f226594932743dc7266b68d5fd363d8
Chris Hyde <c_hyde333@hotmail.com>
2022-12-17 16:57:22 +0000

    Abbreviate a Two Word Name

--- practice.js
+++ practice.js
@@ -2240,3 +2240,52 @@ function printerError(s) {
   var fraction = "/";
   return xString + fraction + yString;
 }
+
+/* Code Wars
+
+Level 8 - Abbreviate a Two Word Name
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+    assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+    assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+    assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+    assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+  });
+});
+*/
+
+function abbrevName(name) {
+  const answer = [name[0]];
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] === " ") {
+      answer.push(name[i + 1]);
+    }
+  }
+  return answer.map((el) => el.toUpperCase()).join(".");
+}
+
+// Alternative Solutions
+
+function abbrevName(name) {
+  const newArray = name.split(" ");
+  return (newArray[0][0] + "." + newArray[1][0]).toUpperCase();
+}
+
+function abbrevName(name) {
+  return name
+    .split(" ")
+    .map((el) => el[0].toUpperCase())
+    .join(".");
+}
