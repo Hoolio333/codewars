a1f63766733d2739cfbc97264a389cbc355e6eee
Chris Hyde <c_hyde333@hotmail.com>
2022-12-18 14:24:08 +0000

    Code Wars Square Every Digit

--- practice.js
+++ practice.js
@@ -2391,3 +2391,56 @@ const disemvowel = (str) => {
   }
   return newStr;
 };
+
+/* Code Wars
+
+Level 7 - Square Every Digit
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+
+describe("Basic tests", () => {
+  
+  it("squareDigits(3212) should equal 9414", () => {
+    assert.strictEqual(squareDigits(3212), 9414);
+  });
+
+  it("squareDigits(2112) should equal 4114", () => {
+    assert.strictEqual(squareDigits(2112), 4114);
+  });
+
+  it("squareDigits(0) should equal 0", () => {
+    assert.strictEqual(squareDigits(0), 0);
+  });
+})
+*/
+
+function squareDigits(num) {
+  let x = num.toString().split("").map(Number);
+  x.forEach((element, i) => (x[i] = element * element));
+  return Number(x.join(""));
+}
+
+// Alternative Solutions
+
+function squareDigits(num) {
+  return Number(
+    ("" + num)
+      .split("")
+      .map(function (val) {
+        return val * val;
+      })
+      .join("")
+  );
+}
+
+function squareDigits(num) {
+  return +num
+    .toString()
+    .split("")
+    .map((i) => i * i)
+    .join("");
+}
