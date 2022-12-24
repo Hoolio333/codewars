623ae1d062f6e9fd3c26d4207c4b17e9d9f890af
Chris Hyde <c_hyde333@hotmail.com>
2022-12-24 09:36:17 +0000

    Code wars - Duplicate encoder

--- practice.js
+++ practice.js
@@ -2674,3 +2674,70 @@ function likes(names) {
     " others like this"
   );
 }
+
+/* Code Wars
+
+Level 6 - Duplicate Encoder
+
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+
+describe("Duplicate Encoder", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(duplicateEncode("din"),"(((");
+    assert.strictEqual(duplicateEncode("recede"),"()()()");
+    assert.strictEqual(duplicateEncode("Success"),")())())","should ignore case");
+    assert.strictEqual(duplicateEncode("(( @"),"))((");
+  });
+});
+*/
+
+function duplicateEncode(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
+}
+
+// Alternative Solutions
+
+function duplicateEncode(word) {
+  var unique = "";
+  word = word.toLowerCase();
+  for (var i = 0; i < word.length; i++) {
+    if (word.lastIndexOf(word[i]) == word.indexOf(word[i])) {
+      unique += "(";
+    } else {
+      unique += ")";
+    }
+  }
+  return unique;
+}
+
+function duplicateEncode(word) {
+  word = word.toLowerCase();
+  return word.replace(/./g, (m) =>
+    word.indexOf(m) == word.lastIndexOf(m) ? "(" : ")"
+  );
+}
+
+function duplicateEncode(word) {
+  var letters = word.toLowerCase().split("");
+  return letters
+    .map(function (c, i) {
+      return letters.some(function (x, j) {
+        return x === c && i !== j;
+      })
+        ? ")"
+        : "(";
+    })
+    .join("");
+}
