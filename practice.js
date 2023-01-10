666e50c27d3354c017b156da14b6448b4d53943d
Chris Hyde <c_hyde333@hotmail.com>
2023-01-10 18:09:09 +0000

    Code Wars - Mumbling

--- practice.js
+++ practice.js
@@ -2790,3 +2790,53 @@ function array_diff(a, b) {
 
   return arr;
 }
+
+/* Code Wars
+
+Level 7 - Mumbling
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+describe("accum",function() {
+it("Basic tests",function() {    
+	Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+	Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+	Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+	Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+	Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+})}) */
+
+function accum(s) {
+  let letters = s.split(""),
+    words = [];
+
+  for (var i = 0; i < letters.length; i++) {
+    words.push(
+      letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase())
+    );
+  }
+
+  return words.join("-");
+}
+
+// Alternative Solutions
+
+function accum(s) {
+  return s
+    .split("")
+    .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
+    .join("-");
+}
+
+function accum(s) {
+  return s
+    .split("")
+    .map((x, index) => x.toUpperCase() + Array(index + 1).join(x.toLowerCase()))
+    .join("-");
+}
