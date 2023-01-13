bd37f2597242206eeb2e28a976cbed572267ca97
Chris Hyde <c_hyde333@hotmail.com>
2023-01-13 14:41:42 +0000

    Code Wars - Century from year

--- practice.js
+++ practice.js
@@ -2966,3 +2966,52 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 
 const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
   mpg * fuelLeft >= distanceToPump;
+
+/* Code Wars
+
+Level 8 - Century From Year
+
+ntroduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(century(1705), 18, 'Testing for year 1705');
+Test.assertEquals(century(1900), 19, 'Testing for year 1900');
+Test.assertEquals(century(1601), 17, 'Testing for year 1601');
+Test.assertEquals(century(2000), 20, 'Testing for year 2000');
+Test.assertEquals(century(89), 1, 'Testing for year 89');
+  });
+});
+*/
+
+function century(year) {
+  let result = 0;
+  for (let i = 0; i < year; i++) {
+    if (i % 100 === 0) {
+      result++;
+    }
+  }
+  return result;
+}
+
+// Alternative Solutions
+
+const century = (year) => Math.ceil(year / 100);
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
+
+function century(year) {
+  return ((year + 99) / 100) | 0;
+}
