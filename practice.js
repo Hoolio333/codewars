7d3ae0ede9c5881501634c5f360ba4ef3870f031
Chris Hyde <c_hyde333@hotmail.com>
2023-01-16 15:38:16 +0000

    Code Wars - Build a pile of Cubes

--- practice.js
+++ practice.js
@@ -3116,3 +3116,81 @@ function even_or_odd(number) {
 }
 
 const even_or_odd = (n) => (n % 2 ? "Odd" : "Even");
+
+/* Code Wars
+
+Level 6 - Build a pile of Cubes
+
+Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n3 n^3n 
+3
+ , the cube above will have volume of (n−1)3 (n-1)^3(n−1) 
+3
+  and so on until the top which will have a volume of 13 1^31 
+3
+ .
+
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as n3+(n−1)3+(n−2)3+...+13=m n^3 + (n-1)^3 + (n-2)^3 + ... + 1^3 = mn 
+3
+ +(n−1) 
+3
+ +(n−2) 
+3
+ +...+1 
+3
+ =m if such a n exists or -1 if there is no such n.
+
+Examples:
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1
+
+it("Basic tests",function() {
+  assert.strictEqual(findNb(4183059834009), 2022)
+  assert.strictEqual(findNb(24723578342962), -1)
+  assert.strictEqual(findNb(135440716410000), 4824)
+  assert.strictEqual(findNb(40539911473216), 3568)
+})
+*/
+
+function findNb(m) {
+  let n = parseInt(Math.sqrt(2 * Math.sqrt(m))) - 1;
+  while (((n * (n + 1)) / 2) ** 2 < m) {
+    n++;
+  }
+  return ((n * (n + 1)) / 2) ** 2 > m ? -1 : n;
+}
+
+// Alternative Solutions:
+
+function findNb(m) {
+  var n = 0;
+  while (m > 0) m -= (++n) ** 3;
+  return m ? -1 : n;
+}
+
+function findNb(m) {
+  let n = 0;
+  let sum = 0;
+  while (sum < m) {
+    n++;
+    sum += Math.pow(n, 3);
+  }
+  return sum === m ? n : -1;
+}
+
+function findNb(m) {
+  m = Math.sqrt(m) * 2;
+  if (m != parseInt(m)) {
+    return -1;
+  }
+  var result = parseInt(Math.sqrt(m));
+  return result * (result + 1) == m ? result : -1;
+}
+
+var findNb = (m) => {
+  var n = Math.floor((4 * m) ** 0.25);
+  var sum = (x) => ((x * (x + 1)) / 2) ** 2;
+  return sum(n) == m ? n : -1;
+};
