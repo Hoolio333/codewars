d10eec02a57d1baa05b5b22c3b61f51e3ff2e378
Chris Hyde <c_hyde333@hotmail.com>
2023-01-18 20:45:26 +0000

    Code Wars Sum of the first nth term of Series

--- practice.js
+++ practice.js
@@ -3233,3 +3233,62 @@ function cockroachSpeed(s) {
 function cockroachSpeed(s) {
   return Math.floor((s * 100000) / 3600);
 }
+
+/* Code Wars
+
+Level 7 - Sum of the first nth term of Series
+
+Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(SeriesSum(1), "1.00")
+Test.assertEquals(SeriesSum(2), "1.25")
+Test.assertEquals(SeriesSum(3), "1.39")
+Test.assertEquals(SeriesSum(4), "1.49")
+  });
+});
+*/
+
+function SeriesSum(n) {
+  let result = 0;
+  let reverage = 1;
+  for (let i = 0; i < n; i += 1) {
+    if (i === 0) {
+      result = 1;
+    } else {
+      reverage += 3;
+      result = result + 1 / reverage;
+    }
+  }
+  return result.toFixed(2);
+}
+
+// Alternative Solutions
+
+function SeriesSum(n) {
+  for (var s = 0, i = 0; i < n; i++) {
+    s += 1 / (1 + i * 3);
+  }
+
+  return s.toFixed(2);
+}
+
+function SeriesSum(n) {
+  for (a = 0, i = 1; i <= n * 3; i += 3) a += 1 / i;
+  return a.toFixed(2);
+}
