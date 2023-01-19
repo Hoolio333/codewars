a870882d298ce481f3dfabde649fa11dee9f56ff
Chris Hyde <c_hyde333@hotmail.com>
2023-01-19 13:27:44 +0000

    Code Wars Keep up the hoop

--- practice.js
+++ practice.js
@@ -3292,3 +3292,51 @@ function SeriesSum(n) {
   for (a = 0, i = 1; i <= n * 3; i += 3) a += 1 / i;
   return a.toFixed(2);
 }
+
+/* Code Wars
+
+Level 8 - Keep up the hoop
+
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(hoopCount(3),"Keep at it until you get it" ) 
+Test.assertEquals(hoopCount(11),"Great, now move on to tricks" )
+  });
+});
+*/
+
+function hoopCount(n) {
+  if (n < 10) return "Keep at it until you get it";
+  else return "Great, now move on to tricks";
+}
+
+// Alternative Solution
+
+function hoopCount(n) {
+  return n < 10
+    ? "Keep at it until you get it"
+    : "Great, now move on to tricks";
+}
+
+const h = (n) =>
+  n > 9 ? "Great, now move on to tricks" : "Keep at it until you get it";
+
+function hoopCount(n) {
+  const HOPS_LIMIT = 10;
+  return n >= HOPS_LIMIT
+    ? "Great, now move on to tricks"
+    : "Keep at it until you get it";
+}
+
+function hoopCount(n) {
+  return n >= 10
+    ? "Great, now move on to tricks"
+    : "Keep at it until you get it";
+}
