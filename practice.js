19cb6341d78243c0e0076600cd32736ab76623e9
Chris Hyde <c_hyde333@hotmail.com>
2023-01-21 11:11:58 +0000

    Code Wars Build Tower

--- practice.js
+++ practice.js
@@ -3476,3 +3476,59 @@ function sumDigPow(a, b) {
   }
   return resultArr;
 }
+
+/* Code Wars
+
+Level 6 - Build Tower
+
+Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertDeepEquals(towerBuilder(1), ["*"]);
+Test.assertDeepEquals(towerBuilder(2), [" * ","***"]);
+Test.assertDeepEquals(towerBuilder(3), ["  *  "," *** ","*****"]);
+  });
+});
+
+*/
+
+function towerBuilder(nFloors) {
+  let space,
+    star,
+    tower = [];
+  for (i = 1; i <= nFloors; i++) {
+    space = " ".repeat(nFloors - i);
+    star = "*".repeat(2 * i - 1);
+    tower.push(`${space}${star}${space}`);
+  }
+  return tower;
+}
+
+// Alternative Solutions
+
+function towerBuilder(n) {
+  return Array.from({ length: n }, function (v, k) {
+    const spaces = " ".repeat(n - k - 1);
+    return spaces + "*".repeat(k + k + 1) + spaces;
+  });
+}
