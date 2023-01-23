218584aab67439e9214151901d7b2a1225c32993
Chris Hyde <c_hyde333@hotmail.com>
2023-01-23 12:41:46 +0000

    Code Wars Delete occurrences of an element if it occurs more than n times

--- practice.js
+++ practice.js
@@ -3627,3 +3627,82 @@ function isValidWalk(walk) {
   walk.forEach((c) => res[c]++);
   return walk.length === 10 && res.n == res.s && res.e == res.w;
 }
+
+/* Code Wars
+
+Level 6 - Enough is enough!
+
+Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times.
+He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+Task
+Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.sameOrderedMembers(deleteNth([20,37,20,21], 1), [20,37,21])
+    assert.sameOrderedMembers(deleteNth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
+  });
+});
+*/
+
+function deleteNth(arr, n) {
+  const result = [];
+  for (const key of arr) {
+    const exist = result.filter((item) => item === key).length;
+    if (exist && exist === n) {
+      continue;
+    } else {
+      result.push(key);
+    }
+  }
+  return result;
+}
+
+// Alternative Solutions
+
+function deleteNth(arr, x) {
+  var cache = {};
+  return arr.filter(function (n) {
+    cache[n] = (cache[n] || 0) + 1;
+    return cache[n] <= x;
+  });
+}
+
+const deleteNth = (a, x) => {
+  let m = {};
+  return a.filter((v) => (m[v] = m[v] + 1 || 1) <= x);
+};
+
+function deleteNth(arr, x) {
+  var obj = {};
+  return arr.filter(function (number) {
+    obj[number] = obj[number] ? obj[number] + 1 : 1;
+    return obj[number] <= x;
+  });
+}
+
+function deleteNth(arr, x) {
+  while (true) {
+    for (var i = 0; i < arr.length; ++i) {
+      var count = 0;
+
+      for (var j = 0; j < arr.length; ++j) {
+        if (arr[i] === arr[j]) {
+          ++count;
+
+          if (count > x) {
+            arr.splice(j, 1);
+            break;
+          }
+        }
+      }
+      if (j !== arr.length) break;
+    }
+    if (i === arr.length) break;
+  }
+
+  return arr;
+}
