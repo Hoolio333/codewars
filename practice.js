a142d855ed5d00d5f0fc5676c7c74c732702500d
Chris Hyde <c_hyde333@hotmail.com>
2023-01-25 17:43:24 +0000

    Code Wars - Detect Pangram

--- practice.js
+++ practice.js
@@ -3769,3 +3769,91 @@ function sumTwoSmallestNumbers(numbers) {
   });
   return numbers[0] + numbers[1];
 }
+
+/* Code Wars
+
+Level 6 - Detect Pangram
+
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+describe("Tests", () => {
+  it("test1", () => {
+    var string = "The quick brown fox jumps over the lazy dog."
+    assert.strictEqual(isPangram(string), true)
+  })
+  it("test2", () => {
+    var string = "This is not a pangram."
+    assert.strictEqual(isPangram(string), false)
+  });
+});
+*/
+
+function isPangram(string) {
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
+    return string.indexOf(x) !== -1;
+  });
+}
+
+function isPangram(string) {
+  return "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .every((x) => string.toLowerCase().includes(x));
+}
+
+function isPangram(string) {
+  return (string.match(/([a-z])(?!.*\1)/gi) || []).length === 26;
+}
+
+function isPangram(string) {
+  const alphabetList = [..."abcdefghijklmnopqrstuvwxyz"];
+
+  return alphabetList.every((letter) => string.toLowerCase().includes(letter));
+}
+
+function isPangram(str) {
+  var s = str.toLowerCase();
+  var letters = "zqxjkvbpygfwmucldrhsnioate";
+  for (var i = 0; i < 26; i++)
+    if (s.indexOf(letters.charAt(i)) == -1) return false;
+  return true;
+}
+
+function isPangram(str) {
+  var alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  str = str.toLowerCase();
+  for (var i = 0; i < alphabet.length; i++) {
+    if (str.indexOf(alphabet[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+}
