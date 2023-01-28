aab8aff34735919334b64da033f2b91233d527b5
Chris Hyde <c_hyde333@hotmail.com>
2023-01-28 21:19:57 +0000

    Code Wars Extract the domain name from a URL

--- practice.js
+++ practice.js
@@ -4049,3 +4049,54 @@ const queueTime = (customers, n) => {
 
   return count;
 };
+
+/* Code Wars
+
+Level 5 - Extract the domain name from a URL
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+describe("Sample test", () => {
+  it("Should pass sample tests", () => {
+    assert.equal(domainName("http://google.com"), "google");
+    assert.equal(domainName("http://google.co.jp"), "google");
+    assert.equal(domainName("www.xakep.ru"), "xakep");
+    assert.equal(domainName("https://youtube.com"), "youtube");
+  })  
+})
+*/
+
+function domainName(url) {
+  let sourceString = url
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace("www.", "")
+    .split(/[/?#]/)[0];
+  let domain = sourceString.split(".")[0];
+  return domain;
+}
+
+// Alternative Solutions
+
+function domainName(url) {
+  url = url.replace("https://", "");
+  url = url.replace("http://", "");
+  url = url.replace("www.", "");
+  return url.split(".")[0];
+}
+
+function domainName(url) {
+  return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+}
+
+function domainName(url) {
+  return url
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace("www.", "")
+    .split(".")[0];
+}
