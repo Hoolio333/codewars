a6c40dfacc447455e651a0c332b3145bb074a286
Chris Hyde <c_hyde333@hotmail.com>
2023-02-12 18:21:26 +0000

    Code Wars Rot13

--- practice.js
+++ practice.js
@@ -4809,4 +4809,122 @@ function twoSum(numbers, target) {
   }
 }
 
+/* Code Wars
 
+Level 5 - Rot13
+
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+describe("Tests", function() {
+  it("Sample tests", function() {
+    for (const [input, expected] of [["test", "grfg"], ["Test", "Grfg"]]) {
+      assert.strictEqual(rot13(input), expected, `Test failed with messsage = '${input}'`);
+    }
+  });
+});
+*/
+
+function rot13(str) {
+  return str.replace( /[A-Za-z]/g , function(c) {
+    return String.fromCharCode( c.charCodeAt(0) + ( c.toUpperCase() <= "M" ? 13 : -13 ) );
+  } );
+}
+
+// Alternative Solutions
+
+function rot13(message) {
+  var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+  return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+}
+
+var codeA = 'A'.charCodeAt(0),
+    codeZ = 'Z'.charCodeAt(0),
+    codea = 'a'.charCodeAt(0),
+    codez = 'z'.charCodeAt(0);
+function rot13(message){
+  return message.split('').map(function(char) { 
+    var code = char.charCodeAt(0);
+    if(codeA <= code && code <= codeZ){
+      return String.fromCharCode(((code - codeA) + 13) % 26 + codeA);
+    } else if(codea <= code && code <= codez){
+      return String.fromCharCode(((code - codea) + 13) % 26 + codea);
+    }
+    return char;
+  }).join('');
+}
+
+function rot13(message) {
+  var abc = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+  return message.replace(/[a-z]/gi, c => abc[abc.indexOf(c) + 13]);
+}
+
+function rot13(message) {
+  const alphabet = {
+    a: 'n',
+    b: 'o',
+    c: 'p',
+    d: 'q',
+    e: 'r',
+    f: 's',
+    g: 't',
+    h: 'u',
+    i: 'v',
+    j: 'w',
+    k: 'x',
+    l: 'y',
+    m: 'z',
+    n: 'a',
+    o: 'b',
+    p: 'c',
+    q: 'd',
+    r: 'e',
+    s: 'f',
+    t: 'g',
+    u: 'h',
+    v: 'i',
+    w: 'j',
+    x: 'k',
+    y: 'l',
+    z: 'm',
+    A: 'N',
+    B: 'O',
+    C: 'P',
+    D: 'Q',
+    E: 'R',
+    F: 'S',
+    G: 'T',
+    H: 'U',
+    I: 'V',
+    J: 'W',
+    K: 'X',
+    L: 'Y',
+    M: 'Z',
+    N: 'A',
+    O: 'B',
+    P: 'C',
+    Q: 'D',
+    R: 'E',
+    S: 'F',
+    T: 'G',
+    U: 'H',
+    V: 'I',
+    W: 'J',
+    X: 'K',
+    Y: 'L',
+    Z: 'M',
+  };
+ 
+  let str = '';
+
+  for(let i = 0; i< message.length; i++) {
+    alphabet[message[i]]
+      ? str += alphabet[message[i]]
+      : str += message[i]
+  }
+
+  return str;
+
+}
