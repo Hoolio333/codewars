4767b529100da72b705839c4a0fc4445f20417b3
Chris Hyde <c_hyde333@hotmail.com>
2023-02-14 13:51:52 +0000

    Code Wars Counting Duplicates

--- practice.js
+++ practice.js
@@ -5021,3 +5021,103 @@ function incrementString(str){
                 }
 }
 
+/* Code Wars
+
+Level 6 - Counting Duplicates
+
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(duplicateCount(""), 0);
+    assert.strictEqual(duplicateCount("abcde"), 0);
+    assert.strictEqual(duplicateCount("aabbcde"), 2);
+    assert.strictEqual(duplicateCount("aabBcde"), 2,"should ignore case");
+    assert.strictEqual(duplicateCount("Indivisibility"), 1)
+    assert.strictEqual(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
+  });
+});
+*/
+
+function duplicateCount(text){
+  let result = 0,
+      soltingObject = {};
+  text.toLowerCase().split('').map(str => {
+      if (!soltingObject.hasOwnProperty(str)) {
+        soltingObject[str] = 0;
+      } else {
+        if (soltingObject[str] === 0) {
+          result += 1;
+        }
+        soltingObject[str] = soltingObject[str] + 1;
+      }
+  });
+  return result;
+}
+
+// Alternative Solutions
+
+function duplicateCount(text){
+  return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
+
+function duplicateCount(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
+
+function duplicateCount(text){
+  var lower = text.toLowerCase();
+  var count = 0;
+  var used = [];
+  
+  lower.split('').forEach(function(letter) {
+    if (!used.includes(letter) && (lower.split(letter).length - 1) > 1) {
+      count++;
+      used.push(letter);
+    }
+  });
+  
+  return count;
+}
+
+function duplicateCount(text){
+  
+  var input = text.toLowerCase().split('');
+  
+  var obj = {};
+  
+  for( var i in input) {
+  	
+  	if(!obj[ input[i] ]){
+  		
+  		obj[ input[i] ] = 1;
+  		
+  	} else{
+  		obj[ input[i] ] += 1;
+  	}	
+  }
+  
+  var result = 0;
+  
+  for( var prop in obj) {
+  	
+    if(obj[prop] > 1){
+  	 result++;
+    }
+  }
+  
+  return result;
+  
+}
