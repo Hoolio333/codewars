fdaf917c992fb676e4a7082a7d6e8d7214809328
Chris Hyde <c_hyde333@hotmail.com>
2023-02-16 18:47:54 +0000

    Code Wars Vowel Count

--- practice.js
+++ practice.js
@@ -5158,3 +5158,51 @@ function digitize(n) {
   return String(n).split('').map(Number).reverse()
 }
 
+/* Code Wars
+
+Level 7 - Vowel Count
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+describe("Vowels Count Tests",function(){
+  it("should return 5 for 'abracadabra'",function(){
+    assert.strictEqual(getCount("abracadabra"), 5) ;
+  });
+});
+*/
+
+function getCount(str) {
+  let vowelsCount = 0
+  const vowels = ["a", "e", "i", "o", "u"]
+  for(let char of str) {
+      if(vowels.includes(char)) {
+          vowelsCount++
+      }
+  }
+
+  return vowelsCount;
+}
+
+// Alternative Solutions
+
+function getCount(str) {
+  return (str.match(/[aeiou]/ig)||[]).length;
+}
+
+function getCount(str) {
+ return str.split('').filter(c => "aeiouAEIOU".includes(c)).length;
+}
+
+function getCount(str) {
+  var vowelsCount = 0;
+  str.split("").forEach(function(x){
+    if(x == "a" | x == "e" | x == "i" | x == "o" | x == "u"){
+      vowelsCount += 1;
+    }
+  });  
+  return vowelsCount;
+}
