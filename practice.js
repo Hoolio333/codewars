c0dec61c804c3d8415bcf4948e2b03a8a4892e3a
Chris Hyde <c_hyde333@hotmail.com>
2023-02-17 22:44:25 +0000

    Code Wars Super Duper Easy

--- practice.js
+++ practice.js
@@ -5205,4 +5205,58 @@ function getCount(str) {
     }
   });  
   return vowelsCount;
+
+/* Code Wars
+
+Level 8 - Super Duper Easy
+
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(problem("hello"), "Error");
+    assert.strictEqual(problem(1), 56);
+    assert.strictEqual(problem(5), 256);
+    assert.strictEqual(problem(0), 6);
+    assert.strictEqual(problem(1.2), 66);
+    assert.strictEqual(problem(3), 156);
+    assert.strictEqual(problem("RyanIsCool"), "Error");
+    assert.strictEqual(problem(-3), -144);
+    assert.strictEqual(problem(""), "Error");
+    assert.strictEqual(problem(0.03), 7.5);
+  })
+})
+*/
+
+function problem(x){
+
+if (typeof x=='number') {
+
+return (x*50)+6;
+} else {
+return "Error";
+}
+}
+
+// Alternative Solutions
+
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
+
+function problem(x){
+  return typeof x === "number" ? x * 50 + 6 : "Error";
+}
+
+function problem(x){
+  return typeof x == 'string' ? 'Error' : (x * 50 + 6);
+}
+
+const problem = x => x === +x ? (x * 50) + 6 :  'Error' 
+
+function problem(x){
+  let sum = x * 50 + 6;
+  if(x > 0 || x < 0 || x === 0)
+  return sum
+  else {
+  return "Error"
+}
 }
