081c10976a7aa814b96d8a4e26837647968aef1c
Chris Hyde <c_hyde333@hotmail.com>
2023-02-18 22:22:57 +0000

    Code Wars Persistent Bugger

--- practice.js
+++ practice.js
@@ -5260,3 +5260,50 @@ function problem(x){
   return "Error"
 }
 }
+
+/* Code Wars
+
+Level 6 - Persistent Bugger.
+
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
+
+describe("Persistent Bugger.", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(persistence(39),3);
+    assert.strictEqual(persistence(4),0);
+    assert.strictEqual(persistence(25),2);
+    assert.strictEqual(persistence(999),4);
+  });
+});
+*/
+
+function persistence(num) {
+  let count = 0;
+  while(num.toString().length > 1){
+    num = num.toString().split('').reduce((acc,el)=>acc *= +el,1)
+    count++
+  }
+  return count
+}
+
+// Alternative Solutions
+
+const persistence = num => {
+  return `${num}`.length > 1 
+    ? 1 + persistence(`${num}`.split('').reduce((a, b) => a * +b)) 
+    : 0;
+}
+
+function persistence(num) {
+   for (var i = 0; num > 9; i++) {
+     num = num.toString().split('').reduce((t, c) => c * t);
+   }
+   return i;
+}
+
