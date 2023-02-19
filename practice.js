974208ea1b5d99433da53159d4532737eb3c0215
Chris Hyde <c_hyde333@hotmail.com>
2023-02-19 19:25:26 +0000

    Code Wars Count characters in your string

--- practice.js
+++ practice.js
@@ -5307,3 +5307,50 @@ function persistence(num) {
    return i;
 }
 
+/* Code Wars
+
+Level 6 - Count characters in your string
+
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+
+describe("Tests", () => {
+  it("should pass sample tests", () => {
+    assert.deepEqual(count("aba"), { a: 2, b: 1 }); 
+    assert.deepEqual(count(""), {});    
+  });
+});
+*/
+
+function count (string) {  
+   let cache={};
+   string.split('').map(v=>cache[v]=cache[v]+1||1)
+   return cache;
+}
+
+// Alternative Solutions
+
+function count (string) {  
+  var count = {};
+  string.split('').forEach(function(s) {
+     count[s] ? count[s]++ : count[s] = 1;
+  });
+  return count;
+}
+
+function count (string) {
+  var result = {};
+  
+  for(let i = 0; i < string.length; i++) {
+    if(result.hasOwnProperty(string[i])){
+      result[string[i]] += 1;
+    } else {
+      result[string[i]] = 1;
+    }
+  }
+  
+  return result;
+}
+
+
