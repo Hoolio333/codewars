4d84b8df0fe10b3c59e539c6a66b4f963552ffe7
Chris Hyde <c_hyde333@hotmail.com>
2023-02-23 17:44:01 +0000

    Code Wars Sort the odd

--- practice.js
+++ practice.js
@@ -5531,3 +5531,92 @@ function factorial(n) {
     f *= n--;
   return f;
 }
+
+/* Code Wars
+
+Level 6 - Sort the odd
+
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+describe("Sample Tests", () => {
+  it("should pass some sample tests", () => {
+    assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+    assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+    assert.deepEqual(sortArray([]),[]);
+  });
+});
+*/
+
+function sortArray(array) {
+  const oddArr = [];
+  const evenArr = [];
+  const result = [];
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i]%2 === 0) {
+      evenArr.push(array[i]);
+    } else {
+      oddArr.push(array[i]);
+    }
+  }
+  oddArr.sort((a, b) => a - b);
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i]%2 === 0) {
+      result.push(evenArr.shift());
+    } else {
+      result.push(oddArr.shift());
+    }
+  }
+  return result;
+}
+
+// Alternative Solutions
+
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+  return array.map((x) => x % 2 ? odd.shift() : x);
+}
+
+function sortArray(array) {
+  var odd = array.filter(elem => elem % 2 !== 0).sort((a, b) => a - b);
+  return array.map(elem => elem % 2 === 0 ? elem : odd.shift());
+}
+
+function sortArray(array) {
+  let oddList = array.filter( num => num%2 ).sort( (a,b) => b-a );
+
+  return array.map( num => {
+    return num%2 ? oddList.pop() : num;
+  });
+}
+
+function sortArray(array) {
+  if(array.length == 0) {
+    return array;
+  }
+
+  for(var i=0;i<array.length-1;i++)
+  {
+    for(var j = i; j < array.length; j++)
+    {
+      if((array[i] != 0) && (array[i] % 2 != 0) &&
+        (array[j] != 0) && (array[j] % 2 != 0))
+        {
+          if(array[i] > array[j])
+          {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+          }
+        }
+    }
+  }
+  
+  return array;
+}
+
