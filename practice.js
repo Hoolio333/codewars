54ad2ca17917470ac3ffae34f143e9bbf779334b
Chris Hyde <c_hyde333@hotmail.com>
2023-02-28 18:13:30 +0000

    Code Wars Find The Capitals

--- practice.js
+++ practice.js
@@ -5620,3 +5620,45 @@ function sortArray(array) {
   return array;
 }
 
+/* Code Wars
+
+Level 6 - Find The Capitals
+
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(capitals('CodEWaRs'), [0,3,4,6] );
+  });
+});
+*/
+
+function capitals(word) {
+  const indexes = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      indexes.push(i);
+    }
+  }
+  return indexes;
+}
+
+// Alternative Solutions:
+
+var capitals = function (word) {
+	return word.split('')
+             .map(function(l, i) { if (l.toUpperCase() === l) return i; })
+             .filter(function(i) { return i != null })
+};
+
+const capitals = word => { return word.match(/[A-Z]/g).map( x => { return word.indexOf(x) }) }
+
+var capitals = function (word) {
+  return word.split('').reduce(function(n, l, i){
+    return /[A-Z]/.test(l) && n.push(i), n;
+  }, []);
+};
+
