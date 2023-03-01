250ce8e0cfc33353826a94d624bd19647b2613f1
Chris Hyde <c_hyde333@hotmail.com>
2023-03-01 13:59:33 +0000

    Sum without highest and lowest number

--- practice.js
+++ practice.js
@@ -5662,3 +5662,76 @@ var capitals = function (word) {
   }, []);
 };
 
+/* Code Wars
+
+Level 8 - Sum without highest and lowest number
+
+Task
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+it("example tests", ()=>{
+  assert.strictEqual( sumArray(null)                     , 0 );
+  assert.strictEqual( sumArray([ ])                      , 0 );
+  assert.strictEqual( sumArray([ 3 ])                    , 0 );
+  assert.strictEqual( sumArray([ 3, 5 ])                 , 0 );
+  assert.strictEqual( sumArray([ 6, 2, 1, 8, 10 ])       , 16 );
+  assert.strictEqual( sumArray([ 0, 1, 6, 10, 10 ])      , 17 );
+  assert.strictEqual( sumArray([ -6, -20, -1, -10, -12 ]), -28 );
+  assert.strictEqual( sumArray([ -6, 20, -1, 10, -12 ])  , 3 );
+});
+*/
+
+function sumArray(array) {
+  // Check if array is empty or has only 1 element
+  if (!array || array.length <= 1) {
+    return 0;
+  }
+  
+  // Sort array in ascending order
+  array.sort(function(a, b){return a-b});
+  
+  // Calculate sum of array elements, excluding first and last elements
+  var sum = 0;
+  for (var i = 1; i < array.length - 1; i++) {
+    sum += array[i];
+  }
+  
+  return sum;
+}
+
+// Alternative Solutions
+
+sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((s, e) => s + e, 0) : 0
+
+function sumArray(array) {
+  return Array.isArray(array) && array.length > 1
+    ? array.reduce((s, n) => s + n, 0) - Math.min(...array) - Math.max(...array)
+    : 0
+}
+
+function sumArray(array) {
+  
+  if (array == null || array.length <= 2) {
+    return 0
+  }
+  
+  var max = Math.max.apply(Math, array);
+  var min = Math.min.apply(Math, array);
+  var sum = 0
+  
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+   }
+
+  return sum - max - min
+}
