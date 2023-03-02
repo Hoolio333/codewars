a4d0d614739828c8d1d31a48b2e693ea68dc716b
Chris Hyde <c_hyde333@hotmail.com>
2023-03-02 13:57:33 +0000

    Code Wars Small Enough

--- practice.js
+++ practice.js
@@ -5735,3 +5735,55 @@ function sumArray(array) {
 
   return sum - max - min
 }
+
+/* Code Wars
+
+Level 7 - Small enough? - Beginner
+
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(smallEnough([66, 101], 200), true);
+    assert.strictEqual(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100), false);
+    assert.strictEqual(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
+    assert.strictEqual(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120), true);
+  })
+})
+*/
+
+function smallEnough(a, limit){
+ return a.every(v=>v<=limit)
+}
+
+// Alternative Solutions
+
+function smallEnough(a, limit){
+  return Math.max(...a) <= limit
+}
+
+smallEnough = (a, l) => a.every(e => e <= l)
+
+function smallEnough(a, limit){
+  for(var i of a){
+    if(i > limit)
+      return false;
+  }
+  return true;
+}
+
+function smallEnough(array, limit){
+    return array.filter( (value) => value > limit ).length > 0 ? false : true;
+}
+
+function smallEnough(a, limit){
+	for (let i = 0; i < a.length; i++){
+  	if (a[i] > limit) {
+    	return false
+    }
+  }
+  return true
+
+}
