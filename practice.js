13f1d3b949b693d3d888af45130588b0f1e8c57f
Chris Hyde <c_hyde333@hotmail.com>
2023-03-05 21:57:30 +0000

    Code Wars String ends with

--- practice.js
+++ practice.js
@@ -5787,3 +5787,46 @@ function smallEnough(a, limit){
   return true
 
 }
+
+/* Code Wars
+
+Level 7 - String ends with?
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(solution('abcde', 'cde'), true)
+Test.assertEquals(solution('abcde', 'abc'), false)
+});
+});
+*/
+
+function solution(str, ending) {
+  if (str.endsWith(ending)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Alternative Solutions
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+const solution = (str, ending) => str.endsWith(ending);
+
+function solution(str, ending){
+  if (typeof(str) != "string" || typeof(ending) != "string")
+    throw "wrong type";
+  if (ending.length>str.length)
+    return false;
+  return str.substr(str.length-ending.length, ending.length) == ending;
+}
