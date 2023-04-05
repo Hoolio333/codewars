b0a25a83e273e1bb0514b14eb3ee674a4d6c5dc3
Chris Hyde <c_hyde333@hotmail.com>
2023-04-05 18:59:02 +0100

    Code Wars Lightsabers - names

--- practice.js
+++ practice.js
@@ -5901,3 +5901,36 @@ describe("validatePIN", function() {
   }
 }
 
+/* Code Wars
+
+Level 8 - How many lightsabers do you own?
+
+Inspired by the development team at Vooza, write the function that
+
+accepts the name of a programmer, and
+returns the number of lightsabers owned by that person.
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+Note: your function should have a default parameter.
+
+For example(Input --> Output):
+
+"anyone else" --> 0
+"Zach" --> 18
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual( howManyLightsabersDoYouOwn(), 0 )
+    assert.strictEqual( howManyLightsabersDoYouOwn("Adam"), 0 )
+    assert.strictEqual( howManyLightsabersDoYouOwn("Zach"), 18 )
+    })
+})
+*/
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name === "Zach") {
+    return 18;
+  } else {
+    return 0;
+  }
+}
